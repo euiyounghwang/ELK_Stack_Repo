17112fea11e061e7eba20b9e22bf55b9648cb92e
Euiyoung Hwang <euiyoung.hwang@gxo.com>
2025-07-18 17:10:28 -0500

    Update

--- upgrade-script/Search-reindexing-script.py
+++ upgrade-script/Search-reindexing-script.py
@@ -551,7 +551,7 @@ if __name__ == "__main__":
             if es_target_index in alias_dict.keys():
                 es_t_client.indices.put_alias(es_target_index, alias_dict.get(es_source_index))
                 aliase_name = alias_dict.get(es_source_index)
-        logging.info(f"Updated aliases to the ES indices [{es_target_index} with aliase ({aliase_name})]..")
+                logging.info(f"Updated aliases to the ES indices [{es_target_index} with aliase ({aliase_name})]..")
     
     EndTime_Job = datetime.now()
     Delay_Time = str((EndTime_Job - StartTime_Job).seconds) + '.' + str((EndTime_Job - StartTime_Job).microseconds).zfill(6)[:2]
