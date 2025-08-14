fc80cbbf6cb14431a00831a953498e6ec9a38eae
Euiyoung Hwang <euiyoung.hwang@gxo.com>
2025-08-14 17:08:08 -0500

    Update

--- init.d/scripts/killps.sh
+++ init.d/scripts/killps.sh
@@ -1,8 +1,22 @@
-PID=$(ps -ef | grep kibana | grep -v grep)
+#PID=$(ps -ef | grep kibana | grep -v grep)
+#test=$?
+#if [ $test -eq 0 ]
+#then
+#  PID=$(ps -ef | grep kibana | grep -v grep | tr -s ' ' | cut -d " " -f2)
+#  echo Killing $PID
+#  kill -9 $PID
+#fi
+
+service_name='/apps/kibana/latest/bin/'
+
+PID=$(ps -ef | grep $service_name | grep -v grep)
 test=$?
 if [ $test -eq 0 ]
 then
-  PID=$(ps -ef | grep kibana | grep -v grep | tr -s ' ' | cut -d " " -f2)
+  PID=$(ps -ef | grep $service_name | grep -v grep | tr -s ' ' | cut -d " " -f2)
   echo Killing $PID
   kill -9 $PID
+else
+  echo Kibana is not Running..
 fi
+
