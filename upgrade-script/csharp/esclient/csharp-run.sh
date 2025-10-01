15e83df1da3613dd7a67ca9f1f041b2b165e7065
Euiyoung Hwang <euiyoung.hwang@gxo.com>
2025-10-01 17:20:34 -0500

    Update

--- upgrade-script/csharp/esclient/csharp-run.sh
+++ upgrade-script/csharp/esclient/csharp-run.sh
@@ -6,6 +6,14 @@ SCRIPTDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 echo "C# Run"
 
+# new "dev" cluster (.env example)
+# CERT_PATH="C:\\dev-es8-ca.pem"
+# ES_NODE_1="https://localhost1:9200"
+# ES_NODE_2="https://localhost2:9200"
+# ES_NODE_3="https://localhost3:9200"
+# BASIC_AUTH_USERNAME="<username>"
+# BASIC_AUTH_PASSWORD="<password>"
+
 # --
 # dotnet new console esclient
 # dotnet add package NEST --version 7.17.4
